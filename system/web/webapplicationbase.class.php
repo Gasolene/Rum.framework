53fce9e5a185987c2335520dfc2e79207bb9cd84
D </>
2013-04-12 11:18:59 -0600

    Update Rum

--- system/web/webapplicationbase.class.php
+++ system/web/webapplicationbase.class.php
@@ -1244,7 +1244,14 @@ No building is needed or allowed in a production environment.</p>
 				ob_start();
 				foreach($_POST as $key=>$value)
 				{
-					print("[{$key}] => {$value}\n");
+					if(is_array($value))
+					{
+						print("[{$key}] => ".serialize($value)."\n");
+					}
+					else
+					{
+						print("[{$key}] => {$value}\n");
+					}
 				}
 				$output = ob_get_clean();
 				\System\Web\HTTPResponse::write( \Rum::escape( $output ));
