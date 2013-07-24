bfbb84626da63304aa8e7ec6be9d79e447818c02
Darnell <darnell@commerx.com>
2013-07-24 12:53:30 -0600

    Add new version of MSSQLDataAdapter

---
+++ system/db/mssql/mssqlquerybuilder.class.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+	 * @license			see /docs/license.txt
+	 * @package			PHPRum
+	 * @author			Darnell Shinbine
+	 * @copyright		Copyright (c) 2013
+	 */
+	namespace System\DB\MSSQL;
+	use System\DB\SQLQueryBuilderBase;
+
+
+	/**
+	 * Represents an SQL Query
+	 *
+	 * @property bool $empty specifies whether to return empty result set
+	 *
+	 * @package			PHPRum
+	 * @subpackage		DB
+	 * @author			Darnell Shinbine
+	 */
+	final class MSSQLQueryBuilder extends SQLQueryBuilderBase
+	{
+		/**
+		 * object opening delimiter
+		 * @var string
+		**/
+		protected $objectOpeningDelimiter = "[";
+
+		/**
+		 * object closing delimiter
+		 * @var string
+		**/
+		protected $objectClosingDelimiter = "]";
+
+		/**
+		 * string delimiter
+		 * @var string
+		**/
+		protected $stringDelimiter = "'";
+	}
+?>
