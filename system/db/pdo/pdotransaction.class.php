49955869b20a17e1ff176a9e82a5184f11917bfc
Darnell <darnell@commerx.com>
2013-07-25 14:00:56 -0600

    Add new version of PDOAdapter

---
+++ system/db/pdo/pdotransaction.class.php
@@ -0,0 +1,47 @@
+<?php
+	/**
+	 * @license			see /docs/license.txt
+	 * @package			PHPRum
+	 * @author			Darnell Shinbine
+	 * @copyright		Copyright (c) 2013
+	 */
+	namespace System\DB\PDO;
+	use \System\DB\TransactionBase;
+
+
+	/**
+	 * Represents an open connection to a PDO
+	 *
+	 * @package			PHPRum
+	 * @subpackage		DB
+	 * @author			Darnell Shinbine
+	 */
+	final class PDOTransaction extends TransactionBase
+	{
+		/**
+		 * Begins a transaction
+		 */
+		protected function beginTransaction()
+		{
+			$this->dataAdapter->pdo->beginTransaction();
+		}
+
+
+		/**
+		 * Implements a rollback
+		 */
+		protected function rollbackTransaction()
+		{
+			$this->dataAdapter->pdo->rollBack();
+		}
+
+
+		/**
+		 * Implements a commit
+		 */
+		protected function commitTransaction()
+		{
+			$this->dataAdapter->pdo->commit();
+		}
+	}
+?>
