5c289d68e97da593cb252a3a197f6cf136952a3c
Darnell <darnell@commerx.com>
2014-01-10 16:46:29 -0700

    Changed Credentials trigger AuthenticateEvent

--- system/security/ldapcredential.class.php
+++ system/security/ldapcredential.class.php
@@ -46,7 +46,7 @@
 							if( $this->checkAccountActive( $ds )) {
 
 								// Raise event
-								\System\Base\ApplicationBase::getInstance()->events->raise(new \System\Base\Events\AuthenticateEvent(), $this, $this->credential);
+								\System\Base\ApplicationBase::getInstance()->events->raise(new \System\Base\Events\AuthenticatedEvent(), $this, array('username'=>$username));
 
 								// Success!
 								return new AuthenticationStatus();
--- system/security/tablecredential.class.php
+++ system/security/tablecredential.class.php
@@ -43,7 +43,7 @@
 							if( $this->checkAccountActive( $ds )) {
 
 								// Raise event
-								\System\Base\ApplicationBase::getInstance()->events->raise(new \System\Base\Events\AuthenticateEvent(), $this, $this->credential);
+								\System\Base\ApplicationBase::getInstance()->events->raise(new \System\Base\Events\AuthenticatedEvent(), $this, $ds->row);
 
 								// Success!
 								return new AuthenticationStatus();
