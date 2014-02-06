bbd0cc15f34787338f2d867a0524b5723a604847
Darnell <darnell@commerx.com>
2014-02-06 11:33:12 -0700

    Added GridViewNotNullFilter

---
+++ system/web/webcontrols/gridviewnotnullfilter.class.php
@@ -0,0 +1,112 @@
+<?php
+	/**
+	 * @license			see /docs/license.txt
+	 * @package			PHPRum
+	 * @author			Darnell Shinbine
+	 * @copyright		Copyright (c) 2013
+	 */
+	namespace System\Web\WebControls;
+
+
+	/**
+	 * Represents a GridView not null filter
+	 *
+	 * @package			PHPRum
+	 * @author			Darnell Shinbine
+	 */
+	class GridViewNotNullFilter extends GridViewFilterBase
+	{
+		/**
+		 * specifies control tool tip
+		 * @var string
+		 */
+		protected $tooltip					= 'Select a option from the list';
+
+
+		/**
+		 * process the HTTP request array
+		 *
+		 * @param  array	&$request	request data
+		 * @return void
+		 */
+		public function requestProcessor( array &$request )
+		{
+			$HTMLControlId = $this->getHTMLControlId();
+
+			if(isset($request[$HTMLControlId . '__filter_value']))
+			{
+				$this->submitted = true;
+				if($request[$HTMLControlId . '__filter_value'])
+				{
+					$this->value = $request[$HTMLControlId . '__filter_value'];
+//					unset($request[$HTMLControlId . '__filter_value']);
+				}
+			}
+		}
+
+
+		/**
+		 * filter DataSet
+		 *
+		 * @param  DataSet	&$ds		DataSet
+		 * @return void
+		 */
+		public function filterDataSet(\System\DB\DataSet &$ds )
+		{
+			if($this->value) {
+				$ds->filter($this->column->dataField, 'not null');
+				if($this->column->gridView->canUpdateView) {
+					$this->column->gridView->needsUpdating = true;
+				}
+			}
+		}
+
+
+		/**
+		 * returns filter text node
+		 * 
+		 * @param  string $requestString a string containing request data
+		 * @return DomObject
+		 */
+		public function getDomObject($requestString)
+		{
+			$HTMLControlId = $this->getHTMLControlId();
+			$uri = \Rum::config()->uri;
+
+			$select = new \System\XML\DomObject( 'select' );
+			$select->setAttribute('name', "{$HTMLControlId}__filter_value");
+			$select->setAttribute('title', $this->tooltip);
+//			$select->setAttribute('class', 'booleanfilter');
+			$option = new \System\XML\DomObject( 'option' );
+			$option->setAttribute('value', '');
+			$option->nodeValue = '';
+			$select->addChild($option);
+
+			// get values
+			foreach(array('not null'=>'false', 'null'=>'true') as $key=>$value)
+			{
+				$option = new \System\XML\DomObject( 'option' );
+				$option->setAttribute('value', $value);
+				$option->nodeValue = $key;
+
+				if($this->value==$value)
+				{
+					$option->setAttribute('selected', 'selected');
+				}
+
+				$select->addChild($option);
+			}
+
+			if($this->column->gridView->ajaxPostBack)
+			{
+				$select->setAttribute( 'onchange', "Rum.evalAsync('{$uri}/', '{$requestString}&{$HTMLControlId}__filter_value='+this.value);" );
+			}
+			else
+			{
+				$select->setAttribute( 'onchange', "Rum.sendSync('{$uri}/', '{$requestString}&{$HTMLControlId}__filter_value='+this.value);" );
+			}
+
+			return $select;
+		}
+	}
+?>
