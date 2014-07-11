58662243e367a8f53a7a9911bb4b6dac098a604f
Darnell <darnell@commerx.com>
2014-07-11 10:53:39 -0600

    Added Date::dateFormat, DateTime::dataFormat, and Time::dataFormat
    Added Date::dateFormat, DateTime::dataFormat, and Time::dataFormat

--- system/web/webcontrols/date.class.php
+++ system/web/webcontrols/date.class.php
@@ -11,6 +11,8 @@
 	/**
 	 * Represents a Date Control
 	 *
+	 * @property string $dateFormat Specifies date format
+	 * 
 	 * @package			PHPRum
 	 * @subpackage		Web
 	 *
@@ -22,6 +24,44 @@
 		 * @ignore
 		 */
 		const type = 'date';
+
+		/**
+		 * specifies date format
+		 * @var string
+		 */
+		protected $dateFormat				= 'Y-m-d';
+
+
+		/**
+		 * gets object property
+		 *
+		 * @param  string	$field		name of field
+		 * @return string				string of variables
+		 * @ignore
+		 */
+		public function __get( $field ) {
+			if( $field === 'dateFormat' ) {
+				return $this->dateFormat;
+			} else {
+				return parent::__get($field);
+			}
+		}
+		
+		/**
+		 * sets object property
+		 *
+		 * @param  string	$field		name of field
+		 * @param  mixed	$value		value of field
+		 * @return mixed
+		 * @ignore
+		 */
+		public function __set( $field, $value ) {
+			if( $field === 'dateFormat' ) {
+				$this->dateFormat = (string)$value;
+			} else {
+				parent::__set( $field, $value );
+			}
+		}
 
 
 		/**
@@ -50,7 +90,7 @@
 		public function getDomObject()
 		{
 			$input = $this->getInputDomObject();
-			$input->setAttribute( 'value', $this->value );
+			$input->setAttribute( 'value', date($this->dateFormat, strtotime($this->value)));
 //			$input->setAttribute( 'class', ' '.self::type );
 			$input->setAttribute( 'type', self::type );
 
--- system/web/webcontrols/datetime.class.php
+++ system/web/webcontrols/datetime.class.php
@@ -11,6 +11,8 @@
 	/**
 	 * Represents a DateTime Control
 	 *
+	 * @property string $dateFormat Specifies date format
+	 *
 	 * @package			PHPRum
 	 * @subpackage		Web
 	 *
@@ -24,6 +26,44 @@
 		const type = 'datetime';
 
 		/**
+		 * specifies date format
+		 * @var string
+		 */
+		protected $dateFormat				= 'Y-m-d';
+
+
+		/**
+		 * gets object property
+		 *
+		 * @param  string	$field		name of field
+		 * @return string				string of variables
+		 * @ignore
+		 */
+		public function __get( $field ) {
+			if( $field === 'dateFormat' ) {
+				return $this->dateFormat;
+			} else {
+				return parent::__get($field);
+			}
+		}
+		
+		/**
+		 * sets object property
+		 *
+		 * @param  string	$field		name of field
+		 * @param  mixed	$value		value of field
+		 * @return mixed
+		 * @ignore
+		 */
+		public function __set( $field, $value ) {
+			if( $field === 'dateFormat' ) {
+				$this->dateFormat = (string)$value;
+			} else {
+				parent::__set( $field, $value );
+			}
+		}
+
+		/**
 		 * getDomObject
 		 *
 		 * returns a DomObject representing control
@@ -33,7 +73,7 @@
 		public function getDomObject()
 		{
 			$input = $this->getInputDomObject();
-			$input->setAttribute( 'value', $this->value );
+			$input->setAttribute( 'value', date($this->dateFormat, strtotime($this->value)));
 //			$input->setAttribute( 'class', ' '.self::type );
 			$input->setAttribute( 'type', self::type );
 
